27eb283c2356d4e27c020ddbf77da44d969cc3d2
oddzye <tzariov.dmitry@yandex.ru>
2022-11-06 01:34:27 +0300

    http-module with nodemon

--- http-module.js
+++ http-module.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello world');
-    res.end();
+    if (req.url === '/') {
+        res.end('Hello page!');
+    }
+    
+    if (req.url === '/about') {
+        res.end('About page');
+    }
+
+    res.end(`
+        <h1>Error page</h1>
+    `);
 });
 
 server.listen(3333);
